d8f63bdaa4207493d6facda4de7db428426698a5
Juan Granda <juanfromecuador@gmail.com>
2016-06-30 16:57:42 -0400

    File IO class complete

--- Library/FileIO.cs
+++ Library/FileIO.cs
@@ -20,32 +20,41 @@ namespace Library
                 if (!File.Exists(path))
                 {
                     //throw appropriate exepction
-                        //implement
+                    //implement
+                    throw new FileNotFoundException();
                 }
 
                 //initialize the empty string we'll be using for each line
                 string line;
 
                 //open a StreamReader to the file specifcied in the path variable 
+                //implement
+                using (StreamReader sr = new StreamReader("Media.txt"))
+                {
+                    //while reading a new from file, addd each line to mediaFile
+                    //as long as each new line is not null
                     //implement
-
-                //while reading a new from file, addd each line to mediaFile
-                //as long as each new line is not null
-                    //implement
-
-
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    sr.Close();
+                }
                 //remember to close your file
                 //file.Close();
+                
             }
             catch (FileNotFoundException)
             {
                 //write out appropriate message
-                    //implement
+                //implement
+                Console.WriteLine("File not found. Check spelling and try again.");
             }
             catch (Exception e)
             {
                 //write out the message of e
-                    //implement
+                //implement
+                Console.WriteLine("Something went wrong");
             }
 
             return mediaFile;
