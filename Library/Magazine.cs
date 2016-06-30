1723a3d3955ab43f33fcaae5c5206cd3a64e7fd0
Juan Granda <juanfromecuador@gmail.com>
2016-06-30 13:52:12 -0400

    Magazine prints media details

--- Library/Magazine.cs
+++ Library/Magazine.cs
@@ -8,27 +8,18 @@ namespace Library
 {
     class Magazine : Media
     {
-        public override int RentalLength
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-
-            set
-            {
-                throw new NotImplementedException();
-            }
-        }
+        public override int RentalLength { get; set; }
 
         public override void PrintMediaDetails()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Magazine {0} ({1})", Title, Length);
+            Console.WriteLine("Rented on: {0}", RentedDate);
+            Console.WriteLine("Due on {0}", ReturnDueDate());
         }
 
         public override DateTime ReturnDueDate()
         {
-            throw new NotImplementedException();
+            return RentedDate.AddDays(RentalLength);
         }
     }
 }
