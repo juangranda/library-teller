2b53a27217e3e50b0801c0a868df7c2e7b0c4ea2
Juan Granda <juanfromecuador@gmail.com>
2016-06-30 10:26:43 -0400

    Regex and DVD class

---
+++ Library/DVD.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class DVD : Media
+    {
+        public override int RentalLength { get; set; }
+
+        public DVD()
+        {
+            RentedDate = DateTime.Today;
+        }
+
+        public override void PrintMediaDetails()
+        {
+
+        }
+
+        public override DateTime ReturnDueDate()
+        {
+            return  ;
+        }
+
+        public override void SetRentedDate()
+        {
+            RentedDate = DateTime.Today;
+        }
+
+    }
+}
